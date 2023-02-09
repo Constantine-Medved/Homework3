422bba18aa0d4d4c2079e3d0cee68727b1d9a4ae
Constantine-Medved <medved.k@outlook.com>
2023-02-09 13:19:31 +0300

    Task 2 - Done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1() {
         byte orangesContainersQuantity = 127;
@@ -11,9 +12,18 @@ public class Main {
         System.out.println("Значение переменной orangesBoxesQuantity с типом int равно " + orangesBoxesQuantity);
         long orangesQuantity = 42949672940L;
         System.out.println("Значение переменной orangesQuantity с типом long равно " + orangesQuantity);
-        float juiceInOneOrange = 0.23333F;
+        float juiceInOneOrange = 0.23333f;
         System.out.println("Значение переменной juiceInOneOrange с типом float равно " + juiceInOneOrange);
         double juiceInOneContainer = 10021590352.66333E+39;
         System.out.println("Значение переменной juiceInOneContainer с типом double равно " + juiceInOneContainer);
+    }
+    public static void task2() {
+        float a = 27.12f;
+        long b = 987678965549L;
+        double c = 2.786;
+        short d = 569;
+        short e = -159;
+        int f = 27897;
+        byte g = 67;
     }
 }
